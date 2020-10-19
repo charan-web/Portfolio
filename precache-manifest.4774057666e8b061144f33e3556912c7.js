fce4fac1f1a65e3139aecd9dc7c62c99ff6e43d3
charan-web <cc710788@gmail.com>
2020-10-19 12:37:56 +0530

    Updates

---
+++ precache-manifest.4774057666e8b061144f33e3556912c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d97b5951d05ed0f504c5e324f71755c",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "ed776d4d594f03f062a3",
+    "url": "/portofolio/static/css/main.f451c248.chunk.css"
+  },
+  {
+    "revision": "0d065977fdb035b85983",
+    "url": "/portofolio/static/js/2.a2f89e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.a2f89e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed776d4d594f03f062a3",
+    "url": "/portofolio/static/js/main.884dd351.chunk.js"
+  },
+  {
+    "revision": "a97695da70344fd75e52",
+    "url": "/portofolio/static/js/runtime-main.fbc8e15e.js"
+  }
+]);
