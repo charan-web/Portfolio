cca1c39f1b6f8ad0fac09a10cb3b462d834f96e3
charan-web <cc710788@gmail.com>
2020-10-21 11:47:15 +0530

    updated and created new branch

--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -12,24 +12,24 @@ const MySkills = () => {
     return (  
         <div className="skillContainer">
             <div>
-                <img src="./skillImages/html.jpg" alt="html"/>
+                <img src={process.env.PUBLIC_URL+"/skillImages/html.jpg"} alt="html"/>
                 <h1>HTML 5</h1>
             </div>
                  
             <div>
-                <img src="/skillImages/css.jpg" alt="css"/>
+                <img src={process.env.PUBLIC_URL+"/skillImages/css.jpg"} alt="css"/>
                 <h1>CSS 3</h1>
             </div>
             <div>
-                <img src="/skillImages/javascript.jpg" alt="javascript"/>
+                <img src={process.env.PUBLIC_URL+"/skillImages/javascript.jpg"} alt="javascript"/>
                 <h1>JAVASCRIPT</h1>
             </div>
             <div>
-                <img src="/skillImages/react.jpg" alt="react"/>
+                <img src={process.env.PUBLIC_URL+"/skillImages/react.jpg"} alt="react"/>
                 <h1>REACT</h1>
             </div>
             <div>
-                <img src='/skillImages/node.jpg' alt=""/>
+                <img src={process.env.PUBLIC_URL+'/skillImages/node.jpg'} alt="noden"/>
                 <h1>NODE</h1>
             </div>
         </div>
